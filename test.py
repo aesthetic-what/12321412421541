3afc0866b04ff079e2c8751a204240196a54cf17
aesthetic-what <ladnoladno477@gmail.com>
2024-09-14 13:29:29 +0500

    Add files via upload

--- test.py
+++ test.py
@@ -1,68 +1,72 @@
 import sys
-from PyQt5.QtCore import QTimer
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtCore import QTimer
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 
 
 class main_window(QMainWindow):
     def __init__(self):
         super().__init__()
+        uic.loadUi("greenlight.ui", self)
         self.timer = QTimer()
         self.timer.timeout.connect(self.traffic_light)
         self.timer.start(2000)
 
+        self.display_timer = QTimer()
+        self.display_timer.timeout.connect(self.update_timer_display)
+        self.timer.start(1000)
+
+
         self.state = 0
+        self.countdown = 2
 
-        uic.loadUi("greenlight.ui", self)
+        self.label.setText(str(self.countdown))
 
-        self.frame.setStyleSheet("background-color: rgb(144, 144, 144)")
-        self.red.setStyleSheet("background-color: red; border-radius: 60px")
-        self.yellow.setStyleSheet("background-color: darkyellow; border-radius: 60px")
-        self.green.setStyleSheet("background-color: rgb(1, 50, 32); border-radius: 60px")
-
-        self.min_frame.setStyleSheet("background-color: rgb(144, 144, 144)")
-        self.min_red.setStyleSheet("background-color: rgb(139, 0, 0); border-radius: 60px")
-        self.min_green.setStyleSheet("background-color: rgb(0, 128, 0); border-radius: 60px")
 
 
     def set_light(self, light, color):
         light.setStyleSheet(f"background-color: {color}; border-radius: 60px")
 
+    def update_timer_display(self):
+        # Обновляем отсчет на экране каждую секунду
+        self.countdown -= 1
+        self.label.setText(str(self.countdown))
+
+        # Если отсчет завершился, сбрасываем значение
+        if self.countdown <= 0:
+            self.countdown = 2
+
 
     def traffic_light(self):
         if self.state == 0:
             self.set_light(self.red, 'red')
-            self.set_light(self.yellow, 'rgb(139, 128, 0)')
-            self.set_light(self.green, 'rgb(1, 50, 32)')
-            self.set_light(self.min_green, 'rgb(0, 128, 0)')
-            self.set_light(self.min_red, 'rgb(139, 0, 0)')
+            self.set_light(self.yellow, 'grey')
+            self.set_light(self.green, 'grey')
+            self.set_light(self.min_green, 'rgb(0, 200, 0)')
+            self.set_light(self.min_red, 'grey')
             self.state = 1
         elif self.state == 1:
             self.set_light(self.red, 'red')
             self.set_light(self.yellow, 'yellow')
-            self.set_light(self.green, 'rgb(1, 50, 32)')
-            self.set_light(self.min_green, 'rgb(0, 128, 0)')
+            self.set_light(self.green, 'grey')
+            self.set_light(self.min_green, 'rgb(0, 200, 0)')
             self.state = 2
         elif self.state == 2:
-            self.set_light(self.red, 'darkred')
+            self.set_light(self.red, 'grey')
             self.set_light(self.yellow, 'yellow')
-            self.set_light(self.green, 'rgb(1, 50, 32)')
-            self.set_light(self.min_green, 'rgb(1, 50, 32)')
-            self.set_light(self.min_red, 'red')
+            self.set_light(self.green, 'grey')
+            self.set_light(self.min_green, 'rgb(0, 200, 0)')
+            self.set_light(self.min_red, 'grey')
             self.state = 3
         elif self.state == 3:
-            self.set_light(self.red, 'darkred')
-            self.set_light(self.yellow, 'rgb(139, 128, 0)')
+            self.set_light(self.red, 'grey')
+            self.set_light(self.yellow, 'grey')
             self.set_light(self.green, 'green')
             self.set_light(self.min_red, 'red')
-            self.set_light(self.min_green, 'rgb(1, 50, 32)')
+            self.set_light(self.min_green, 'grey')
             self.state = 0
-
-
-
-
-
-
+        
+        self.countdown = 2
 
 
 if __name__ == "__main__":
